e1b52cb1e272ca8b4f70238e21a0a37ceca8d8c9
krylov72 <qobject1337@gmail.com>
2024-04-08 00:28:16 +0500

    add sections to main App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './layout/header/Header';
+import { Main } from './layout/section/main/Main';
+import { AboutMe } from './layout/section/about me/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <AboutMe />
     </div>
   );
 }
