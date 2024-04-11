5c5d5c3429f308f09f9c8b3487dee085d67eefad
krylov72 <qobject1337@gmail.com>
2024-04-11 14:59:50 +0500

    add types

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -3,12 +3,17 @@ import sprite from '../../assets/images/icons-sprite.svg';
 
 type IconPropsType = {
     iconSrc: string
+    color?: string
+    width?: string
+    height?: string
+    viewbox?: string
+    selected?: boolean
 }
 
 export const Icon = (props:IconPropsType) => {
     return (
         <div>
-            <svg width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width={props.width || '20px'} height={props.height || '20px'} viewBox={props.viewbox || '0 0 22 22'} fill="none" xmlns="http://www.w3.org/2000/svg" color={props.color}>
                 <use xlinkHref={ `${sprite}#${props.iconSrc}`}></use>
             </svg> 
         </div>
