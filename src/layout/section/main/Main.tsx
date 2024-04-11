119ba66e418a1f50d672228522a86e6463a92cc5
krylov72 <qobject1337@gmail.com>
2024-04-11 15:01:12 +0500

    refactoring

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -1,40 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
 import mainImage from '../../../assets/images/main.webp';
+import { Container } from '../../../components/Container';
+import { ColorTheme } from '../../../theme/Theme';
 
 export const Main = () => {
     return (
-        <div>
-            <StyledMain>
-                <TextBlock>
-                <Name>Denis Novik</Name>
-                <Description>UX | UI designer 24 years old, Minsk</Description>
+        <StyledMain>
+            <Container>
+            <TextBlock>
+                <Name>Denis<br></br>Novik</Name>
+                <Description>UX | UI designer<br></br>24 years old, Minsk</Description>
                 <LangSlider>
                     <Lang href="">RU|ENG</Lang>
                 </LangSlider>
                 </TextBlock>
                 <MainPhoto src={mainImage} />
-            </StyledMain>
-        </div>
+            </Container>
+                
+        </StyledMain>
     );
 };
 
 const StyledMain = styled.main`
-    min-height:100vh;
+    min-height:60vh;
     display:block;
     justify-content:space-between;
     align-items:center;
+    padding-top:40px;
 
 `
 const TextBlock = styled.div`
     display:flex;
     justify-content:space-between;
+    align-items:center;
+    padding-bottom:30px;
 `
 const Name = styled.h2`
-    
+    color: ${ColorTheme.fontcolors.mainfont};
+    font-size: 47px;
+    font-weight: 800;
+    line-height: -1px;
+    text-align: left;
 `
-const Description = styled.p`
-    
+const Description = styled.h1`
+    color: ${ColorTheme.fontcolors.mainfont};
+    font-size: 18px;
+    font-weight: 400;
+    line-height: -1px;
+    align-self:flex-end;
+    text-align:center;
 `
 const LangSlider = styled.div`
     
