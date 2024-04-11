32ea5a245711ce782c2d1c75fb3c354a2693e7ac
krylov72 <qobject1337@gmail.com>
2024-04-11 14:58:01 +0500

    add sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Header } from './layout/header/Header';
 import { Main } from './layout/section/main/Main';
 import { AboutMe } from './layout/section/about me/AboutMe';
+import { Skills } from './layout/section/skills/Skills';
+import { Portfolio } from './layout/section/portfolio/Portfolio';
+import { Contacts } from './layout/section/contacts/Contacts';
+import { Footer } from './layout/footer/Footer';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
       <Header />
       <Main />
       <AboutMe />
+      <Skills />
+      <Portfolio />
+      <Contacts />
+      <Footer />
     </div>
   );
 }
