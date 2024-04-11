7e9fc42cad90888def31b352b3ce3f13087c37ec
krylov72 <qobject1337@gmail.com>
2024-04-11 15:00:49 +0500

    create

--- src/layout/section/about me/AboutMe.tsx
+++ src/layout/section/about me/AboutMe.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { SectionTitle } from '../../../components/SectionTitle';
+import styled from 'styled-components';
 
 export const AboutMe = () => {
     return (
-        <div>
-            
-        </div>
+        <StyledAboutMe>
+            <FlexWrapper justify='center' direction='column' align='center' >
+                <SectionTitle title='About me' />
+                <Text>Hi, I'm Denis – UX/UI designer from Minsk.I'm interested in design and everything connected with it.<br></br><br /><br />I'm studying at courses "Web and mobile design interfaces" in IT-Academy.<br /><br /><br />Ready to implement excellent projects with wonderful people.</Text>
+            </FlexWrapper>
+        </StyledAboutMe>
     );
 };
+
+const StyledAboutMe = styled.section`
+    padding-top:120px;
+    display:flex;
+    justify-content:center;
+    &>${FlexWrapper} {
+        max-width:485px;
+        width:100%;
+    }
+`
+
+export const Text = styled.p`
+    color: rgb(7, 7, 7);
+    font-size: 18px;
+    font-weight: 400;
+    line-height: -1px;
+`
+
